d494a6749bab323e07b8a9adb8755fd04be4eb2c
Vanya <vanya.ilyin@yahoo.com>
2011-09-02 17:43:10 +0400

    comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rznmarket::Application.config.secret_token = 'dce23533b8d057b252dd619886d170fbd4bb67be470e60a3a15405bd229e1e71a5f2c893b7805b5cedbf0404aa7becc5a374c112d425660abd4ef86441456844'
